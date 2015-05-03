4513680c4a97540e437b72982f0930bba4ef98b3
read-only-man <maeda.ru@gmail.com>
2015-05-04 00:36:04 +0900

    20150503

---
+++ 20150504/print-svg.lisp
@@ -0,0 +1,46 @@
+(defmacro let1 (var val &body body)
+  `(let ((,var ,val))
+     ,@body))
+
+(defmacro split (val yes no)
+  (let1 g (gensym)
+    `(let1 ,g ,val
+	   (if ,g
+	     (let ((head (car ,g))
+		   (tail (cdr ,g)))
+	       ,yes)
+	     ,no))))
+
+(defun pairs (lst)
+  (labels ((f (lst acc)
+	      (split lst
+		     (if tail
+		       (f (cdr tail) (cons (cons head (car tail)) acc))
+		       (reverse acc))
+		     (reverse acc))))
+    (f lst nil)))
+
+(defun print-tag (name alst closingp)
+  (princ #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (att)
+	  (format t " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
+	alst)
+  (princ #\>))
+
+;(print-tag 'mytag '((color . blue) (height . 9)) nil)
+
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+		     (list ,@(mapcar (lambda (x)
+				       `(cons ',(car x) ,(cdr x)))
+				     (pairs atts)))
+		     nil)
+	  ,@body
+	  (print-tag ',name nil t)))
+
+(tag mytag (color 'blue height (+ 4 5)))
+;(macroexpand (tag mytag (color 'blue height (+ 4 5))))
+
