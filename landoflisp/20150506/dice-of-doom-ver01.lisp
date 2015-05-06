8a493172ccd978df49c94bc0592bac3d1dd119d3
read-only-man <maeda.ru@gmail.com>
2015-05-07 00:07:51 +0900

    2015050602

--- landoflisp/20150506/dice-of-doom-ver01.lisp
+++ landoflisp/20150506/dice-of-doom-ver01.lisp
@@ -33,37 +33,6 @@
 			  first-move
 			  (attacking-moves board player spare-dice))))
 
-(defun add-passing-move (board player spare-dice first-move moves)
-  (if first-move
-    moves
-    (cons (list nil
-		(game-tree (add-new-dice board player (1- spare-dice))
-			   (mod (1+ player) *num-players*)
-			   0
-			   t))
-	  moves)))
-
-(defun attacking-moves (board cur-player spare-dice)
-  (labels ((player (pos)
-		   (car (aref board pos)))
-	   (dice (pos)
-		 (cadr (aref board pos))))
-    (mapcan (lambda (src)
-	      (when (eq (player src) cur-player)
-		(mapcan (lambda (dst)
-			  (when (and (not (eq (player dst) cur-player))
-				     (> (dice src) (dice dst)))
-			    (list
-			      (list (list src dst)
-				    (game-tree (board-attack board cur-player
-							     src dst (dice src))
-					       cur-player
-					       (+ spare-dice (dice dst))
-					       nil)))))
-			(neighbors src))))
-	    (loop for n below *board-hexnum*
-		  collect n))))
-
 (defun neighbors (pos)
   (let ((up (- pos *board-size*))
 	(down (+ pos *board-size*)))
@@ -82,45 +51,10 @@
 				   ((eq pos dst) (list player (1- dice)))
 				   (t hex)))))
 
-#|
-(defun add-new-dice (board player spare-dice)
-  (labels ((f (lst n)
-	      (cond ((null lst) nil)
-		    ((zerop n) lst)
-		    (t (let ((cur-player (caar lst))
-			     (cur-dice (cadar lst)))
-			 (if (and (eq cur-player player) (< cur-dice *max-dice*))
-			   (cons (list cur-player (1+ cur-dice))
-				 (f (cdr lst) (1- n)))
-			   (cons (car lst) (f (cdr lst) n))))))))
-    (board-array (f (coerce board 'list) spare-dice))))
-|#
-
-(defun play-vs-human (tree)
-  (print-into tree)
-  (if (caddr tree)
-    (play-vs-human (handle-human tree))
-    (announce-winner (cadr tree))))
-
 (defun print-into (tree)
   (fresh-line)
   (format t "current player = ~a" (player-letter (car tree)))
   (draw-board (cadr tree)))
-
-(defun handle-human (tree)
-  (fresh-line)
-  (princ "choose your move:")
-  (let ((moves (caddr tree)))
-    (loop for move in moves
-	  for n from 1
-	  do (let ((action (car move)))
-	       (fresh-line)
-	       (format t "~a. " n)
-	       (if action
-		 (format t "~a -> ~a" (car action) (cadr action))
-		 (princ "end turn"))))
-    (fresh-line)
-    (cadr (nth (1- (read)) moves))))
 
 (defun winners (board)
   (let* ((tally (loop for hex across board
@@ -141,53 +75,11 @@
       (format t "The game is a tie between ~a" (mapcar #'player-letter w))
       (format t "The winner is ~a" (player-letter (car w))))))
 
-;(play-vs-human (game-tree (gen-board) 0 0 t))
-
-(defun rate-position (tree player)
-  (let ((moves (caddr tree)))
-    (if moves
-      (apply (if (eq (car tree) player)
-	       #'max
-	       #'min)
-	     (get-ratings tree player))
-      (let ((w (winners (cadr tree))))
-	(if (member player w)
-	  (/ 1 (length w))
-	  0)))))
-
-(defun get-ratings (tree player)
-  (mapcar (lambda (move)
-	    (rate-position (cadr move) player))
-	  (caddr tree)))
-
-
-(defun handle-computer (tree)
-  (let ((ratings (get-ratings tree (car tree))))
-    (cadr (nth (position (apply #'max ratings) ratings) (caddr tree)))))
-
-(defun play-vs-computer (tree)
-  (print-into tree)
-  (cond ((null (caddr tree)) (announce-winner (cadr tree)))
-	((zerop (car tree)) (play-vs-computer (handle-human tree)))
-	(t (play-vs-computer (handle-computer tree)))))
-
-;(play-vs-computer (game-tree (gen-board) 0 0 t))
-
 (let ((old-game-tree (symbol-function 'game-tree))
       (previous (make-hash-table :test #'equalp)))
   (defun game-tree (&rest rest)
     (or (gethash rest previous)
 	(setf (gethash rest previous) (apply old-game-tree rest)))))
-
-(let ((old-rate-position (symbol-function 'rate-position))
-      (previous (make-hash-table)))
-  (defun rate-position (tree player)
-    (let ((tab (gethash player previous)))
-      (unless tab
-	(setf tab (setf (gethash player previous) (make-hash-table))))
-      (or (gethash tree tab)
-	  (setf (gethash tree tab)
-		(funcall old-rate-position tree player))))))
 
 (defun add-new-dice (board player spare-dice)
   (labels ((f (lst n acc)
